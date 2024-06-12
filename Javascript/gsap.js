c63d0b0793877c3e3a5e1d79fdac67b9ecdc6022
Twan <71022124+twan102018@users.noreply.github.com>
2024-06-12 12:22:00 +0200

    update

---
+++ Javascript/gsap.js
@@ -0,0 +1,3 @@
+gsap.to("#container", {opacity: 1, duration: .5, delay: .5});
+gsap.from("#Button", {y: -100, opacity: 0, duration: .5, delay: 1});
+gsap.from("#startButton", {scale: 0, opacity: 0, duration: 0.5, delay: 2});
